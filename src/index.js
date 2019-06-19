7873628e38231ec13dbda55e24e661909140da53
shikha jhamb <shikha.jhamb1992@gmail.com>
2019-06-19 05:41:22 +0530

    Added redux structure

--- src/index.js
+++ src/index.js
@@ -1,7 +1,12 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import { BrowserRouter, Route, Switch, Link, NavLink } from "react-router-dom";
+import configureStore from "./store/configStore";
 import "./styles/style.scss";
-import AppRouter from "./portfolio/AppRouter";
+import AppRouter from "./routers/AppRouter";
+
+const store = configureStore();
+
+console.log(store.getState());
 
 ReactDOM.render(<AppRouter />, document.getElementById("root"));
